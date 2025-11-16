880b9a273c12ac8c58b3ca8876df703c012e4a30
Pantelis Galanos <t8240023@aueb.gr>
2025-11-16 18:54:56 +0200

    Changes

--- Budget.java
+++ Budget.java
@@ -1,5 +1,8 @@
+import.java.util.Scanner;
 public class Budget {
     public static void main( String [] args ) {
-        
+        scanner Scanner = new Scanner
+        System.out.println("Enter the Budjet Year")
+        int year = scanner.nextInt();
     }
 }
