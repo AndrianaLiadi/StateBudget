c837d43d0d69c34f63ea7e8f435608d5cb19ea4b
Ioanna Seimeni <t8240125@aueb.gr>
2025-11-18 23:32:21 +0200

    arxikos kwdikas BudgetService

--- BudgetService.java
+++ BudgetService.java
@@ -1,22 +1,62 @@
 public class BudgetService {
-    private float originalFPA = 0.23f;
-    private float originalbudget = 5000000.67f;
-    private float originaltaktikos = 3000000.41f;
-    private float originalependysewn = originalbudget - originaltaktikos;
-    public float changeFPA(float originalFPA) {
-        return originalFPA*2;
+    private final BudgetDataLoader objloader;
+
+    public BudgetService(BudgetDataLoader objloader) {
+        this.objloader = objloader;
     }
 
-    public float refresh(float originalbudget) {
-        return originalbudget*2;
+    public Budget loadBudget(int year) {
+        String filename = 'budget_' + year + '.json'; //analoga pws tha ta paroume
+        //edw xrhsimopoiw th BudgetDataLoader gia ton xrono pou thelw
+        Budget budget = objloader.loadfromJSON(filename); //analoga pali
+        return budget;
     }
 
-    public float taktikos(float originaltaktikos) {
-        return originaltaktikos*2;
+    public double calculateDeficit(Budget budgetnow) {
+        return budget.getDeficitOrSurplus();
+        //xrhsimopoiw methodo apo to 1 
     }
 
-    public float ependysewn(float originalependysewn) {
-        return originalependysewn*2;
+    public List<BudgetChange> compareBudgets(Budget budgetA, Budget budgetB) {
+        List<BudgetChange> differences = new ArrayList<>;
+        List<String> processedCodes = new ArrayList<>; //wste na mhn ginetai dyo fores
+        for (BudgetItem itemA : budgetA.getItems()) {
+            String code = itemA.getCode();
+            processedCodes.add(code); //exei xrhsimopoihthei
+            BudgetItem itemB = budgetB.getItemByCode(code);
+            double amountA = itemA.getAmount();
+            double amountB;
+            if (itemB != null) {
+                amountB = itemB.getAmount();
+            } else {
+                amountB = 0.0;
+            }
+            //edw prepei na dw ton kwdika tou BudgetChange
+            if (amountA != amountB) {
+                BudgetChange change = new BudgetChange(code, amountA, amountB);
+                differences.add(change);
+            }
+        }
+        for (BudgetItem itemB : budgetB.getItems()) {
+            String code = itemB.getCode();
+            //elegxw thn periptwsh sto budgetB na yparxei kati pou den yparxei sto A
+            if (!processedCodes.contains(code)) {
+                double amountA = 0.0
+                double amountB = itemB.getAmount();
+                //edw pali prepei na dw ton kwdika tou BudgetChange
+                BudgetChange change = new BudgetChange(code, amountA, amountB);
+                differences.add(change);
+            }
+
+        }
+        return differences;
     }
 
+    private final ReportGenerator reportGenerator;
+    public String analyzeImpact(Budget base, Budget modified) {
+        Scenario tempScenario = new Scenario; //prepei na dw ton kwdika kai ton kataskevasth tou Scenario
+        tempScenario.setModifiedBudget(modified);
+        String summary = reportGenerator.generateSummary(tempScenario);
+        return summary;
+    }
 }
