bb5a617265a8c5d7fa37117d7e14995ea15bbfb3
Pantelis Galanos <t8240023@aueb.gr>
2025-11-23 18:03:22 +0200

    Nees prosthikes

--- Budget.java
+++ Budget.java
@@ -1,14 +1,43 @@
 import.java.util.Scanner;
-public class Budget {
+public class Budget{
+    public static void listMaker(){
+        List<BudgetItem> allItems = budget.getItems();
+    }
+
     public static int getYear() {
         scanner Scanner = new Scanner
         System.out.println("Enter the Budjet Year")
         int year = scanner.nextInt();
         return year 
     }
-    public static double total(){
 
+    public static void totalRevenue(){
+        public int trevenue = 0;
+        List<BudgetItem> revenue = new ArrayList<>();
+        for (BudgetItem item : allItems){
+            if (item.getType() == BudgetItem.Type.REVENUE){
+                revenues.add(item);
+                trevenue = trevenue + item;
+            }
+        }
     }
+
+    public static void totalExpenditure(){
+        public int texpenditure = 0;
+        List<BudgetItem> expenditure = new ArrayList<>();
+        for (BudgetItem item : allItems){
+            if (item.getType() == BudgetItem.Type.EXPENDITURE){
+                expeditures.add(item);
+                texpenditure = texpenditure + item;
+            }
+        }
+    }
+
+    public static int deficitFinder(int trevenue, int texpenditure){
+        int deficit = trevenue - texpenditure;
+        return deficit;
+    }
+    
     public static double getItembyCode(){
         scanner Scanner = new Scanner
         System.out.println("Enter the Code of the Item")
