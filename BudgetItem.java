7a650e202ff23dfe081f28846210d90a34ab8f07
Pantelis Galanos <t8240023@aueb.gr>
2025-11-23 20:44:01 +0200

    orismos antikeimenwn

--- BudgetItem.java
+++ BudgetItem.java
@@ -1,3 +1,32 @@
 public class BudgetItem{
     
+    private String code;
+    private String name;
+    private String type;
+    private double amount;
+
+    public BudgetItem(String code, String name, String type, double amount){
+        this.code = code;
+        this.name = name;
+        this.type = type;
+        this.amount = amount;
+    }
+
+    public static String getCode(){
+        return code;
+    }
+
+    public static String getName(){
+        return code;
+    }
+     
+    public static String getType(){
+        return type;
+    }
+
+    public static double getAmount(){
+        return amount;
+    }
+
+    
 }
