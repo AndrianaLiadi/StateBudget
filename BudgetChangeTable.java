420cb7463cba8ec8dd095deafa10e2b450a53d90
= <=>
2025-11-24 21:54:42 +0200

    dior

---
+++ BudgetChangeTable.java
@@ -0,0 +1,23 @@
+package ui; 
+
+
+import model.BudgetChange;
+import java.util.List;
+
+
+public class BudgetChangeTable {
+
+
+List<BudgetChange> changes; 
+
+public BudgetChangeTable(List<BudgetChange> changes) {
+this.changes = changes;
+}
+public void printTable() {
+System.out.println(" ΑΛΛΑΓΜΕΝΟΣ ΠΙΝΑΚΑΣ ");
+for (BudgetChange ch : changes) {
+System.out.println(ch.getCode() + " : " + ch.getName() + " Από " + ch.getAmountA() + " -> " + ch.getAmountB());
+}
+System.out.println("Σύνολο αλλαγών: " + changes.size());
+}
+}
