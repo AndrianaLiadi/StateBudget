0a7750294f3f416a417c73d460795c9f5410c8cf
Pantelis Galanos <t8240023@aueb.gr>
2025-12-07 21:57:03 +0200

    neo

---
+++ src/main/java/model/Budget.java
@@ -0,0 +1,100 @@
+package src.main.java.model;
+
+import src.main.java.model.BudgetItem;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Budget implements Cloneable{
+
+    private List<BudgetItem> items;
+    private int year;
+
+    public Budget(int year, List<BudgetItem> items){
+        this.items = items;
+        this.year = year;
+    }
+
+    public List<BudgetItem> getItems() {
+        return this.items;
+    }
+
+    public List<BudgetItem> listMaker() {
+        return this.getItems();
+    }
+
+    public static int getYear() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the Budget Year");
+        int year = scanner.nextInt();
+        return year; 
+    }
+
+    public long totalRevenue(){
+        long trevenue = 0;
+        List<BudgetItem> revenue = new ArrayList<>();
+         for (BudgetItem item : this.items) {
+            if (item.getType().equals("REVENUE")) {
+                revenue.add(item);
+                trevenue = trevenue + item.getAmount();
+            }
+        }
+        return trevenue;
+    }
+
+    public double totalExpenditure(){
+        long texpenditure = 0;
+        List<BudgetItem> expenditure = new ArrayList<>();
+        for (BudgetItem item : this.items){
+            if (item.getType().equals("EXPENDITURE")){
+                expenditure.add(item);
+                texpenditure = texpenditure + item.getAmount();
+            }
+        }
+        return texpenditure;
+    }
+
+    public double surplusdeficitFinder(double trevenue, double texpenditure){
+        if (trevenue > texpenditure){
+            double surplus = trevenue - texpenditure;
+            System.out.println("The surplus is" + surplus);
+            return surplus;
+        } else {
+            double deficit = texpenditure - trevenue;
+            System.out.println("The deficit is " + deficit);
+            return deficit;
+        }
+    }
+    
+    public BudgetItem getItembyCode(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the Code of the Item");
+        String code = scanner.nextLine();
+        BudgetItem f = null;
+        for (BudgetItem item : this.getItems()){
+            if (item.getCode().equals(code)) {
+                f = item;
+                break;
+            }
+        }
+        return f;
+    }
+
+    @Override
+    public Budget clone() {
+        try {
+            Budget cloned = (Budget) super.clone();
+
+            cloned.items = new ArrayList<>();
+            for (BudgetItem item : this.items) {
+                cloned.items.add(item.clone());
+            }
+
+            return cloned;
+
+        } catch (CloneNotSupportedException e) {
+            System.out.println("Clone Error");
+            return null;
+        }
+    }
+}
