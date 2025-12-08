b23a8b785aae20257146403c2e6bda570bc00b1f
AlexandrosMinakakis <t8240227@aueb.gr>
2025-12-08 21:09:33 +0200

    teleftea allagh

--- src/main/java/ui/ReportGenerator.java
+++ src/main/java/ui/ReportGenerator.java
@@ -7,16 +7,18 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.List;
+import java.io.IOException;
 
 public class ReportGenerator {
 
 public String generateSummary(Scenario scenario){
-    StringBuilder sb = =new StringBuilder();
-    LIst<BUdgetChange> changes = scenario.getChanges();
+    StringBuilder sb = new StringBuilder();
+    List<BudgetChange> changes = scenario.getChanges();
     sb.append("ΑΝΑΦΟΡΑ ΣΕΝΑΡΙΟΥ:"+scenario.getNAme()+ "\n");
     sb.append("--------------\n");
 
-    if (changes == null){
+    if (changes == null|| changes.isEmpty()){
         sb.append("Δεν έχουν γίνει αλλαγές")
         return sb.toString();
     }
@@ -28,22 +30,21 @@ public String generateSummary(Scenario scenario){
 
         sb.append("Παλιό Ποσό:"+ change.getOldAmount()+"\n");
         sb.append("Νέο Πόσο:"+ change.getNewAmount()+ "\n");
-        sb.append("Διαφορά: "+ changegetDifference()+ "\n");
-        sb.append("Ποσοστό: "change.getPercentageChange()+ "%\n");
+        sb.append("Διαφορά: "+ change.getDifference()+ "\n");
+        sb.append("Ποσοστό: "+ change.getPercentageChange()+ "%\n");
 
         sb.append("-------------\n");
     }
     return sb.toString();
 }
 public void exportToTextFile(Scenario scenario, String filename){
-    try{
-        FileWriter writer = new FileWriter(filename);
+    try(FileWriter writer = new FileWriter(filename)){
         writer.write(generateSummary(scenario));
-        System.out.println("Αποθηκεύτηκε στο αρχείο:" filename);
+        System.out.println("Αποθηκεύτηκε στο αρχείο:"+ filename);
     }catch (IOException e) {
         System.out.println("Σφάλμα κατά την εγγραφή του αρχείου.")
         e.printStackTrace();//Εκτύπωση του εγγράγου
     }
 }
-}
+
 
