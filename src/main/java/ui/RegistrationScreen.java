08ab5d986485365f86198e776ae33ede55940f54
= <=>
2025-12-08 11:19:31 +0200

    registration screen

---
+++ src/main/java/ui/RegistrationScreen.java
@@ -0,0 +1,33 @@
+package src.main.java.ui;
+
+import controller.AppController;
+import javax.swing.*;
+import java.awt.*;
+
+public class RegistrationScreen extends JPanel {
+
+    private JTextField nameField;
+    private JTextField surnameField;
+    private JTextField emailField;
+    private JButton registerBtn;
+
+    public RegistrationScreen(AppController controller) {
+
+        setLayout(new GridLayout(4, 1, 10, 10));
+
+        nameField = new JTextField("Name");
+        surnameField = new JTextField("Surname");
+        emailField = new JTextField("Email");
+        registerBtn = new JButton("Register");
+
+        registerBtn.addActionListener(e -> {
+            controller.showScreen(AppController.LANDING);
+        });
+
+        add(nameField);
+        add(surnameField);
+        add(emailField);
+        add(registerBtn);
+    }
+}
+
