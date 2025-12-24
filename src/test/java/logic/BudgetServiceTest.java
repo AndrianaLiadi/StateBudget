f5118aa0dc803b72abb1e773f1f64873d9183601
Ioanna Seimeni <t8240125@aueb.gr>
2025-12-24 11:58:36 +0200

    Add imports

---
+++ src/test/java/logic/BudgetServiceTest.java
@@ -0,0 +1,17 @@
+package logic;
+
+import model.Budget;
+import model.BudgetChange;
+import model.BudgetItem;
+import ui.ReportGenerator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class BudgetServiceTest {
+    
+}
