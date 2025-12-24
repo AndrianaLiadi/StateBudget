00ee8fe91c1ff2f8f9d481bccbb2926de4740725
Ioanna Seimeni <t8240125@aueb.gr>
2025-12-24 12:12:54 +0200

    First Test

--- src/test/java/logic/BudgetServiceTest.java
+++ src/test/java/logic/BudgetServiceTest.java
@@ -21,21 +21,19 @@ public class BudgetServiceTest {
         reportGenerator = new ReportGenerator();
         budgetService = new BudgetService(null, reportGenerator);
     }
-
+    
     @Test
     void testCalculateSurplusOrDeficit() {
+        
         List<BudgetItem> items = new ArrayList<>();
-        
-        //Adding revenue data based on csv file for 2025
-        items.add(new BudgetItem("11", "Φόροι", "REVENUE", 60_000_000_000L)); 
-        items.add(new BudgetItem("14", "Πωλήσεις αγαθών και υπηρεσιών", "REVENUE", 2_000_000_000L));
-        
-        //Adding expenditure data
-        items.add(new BudgetItem("21", "Παροχές σε εργαζομένους", "EXPENDITURE", 15_000_000_000L));
-        items.add(new BudgetItem("22", "Κοινωνικές παροχές", "EXPENDITURE", 40_000_000_000L));
-        Budget budget = new Budget(2025, items);
+        items.add(new BudgetItem("11", "Φόροι", "REVENUE", 56_597_000_000L));
+        items.add(new BudgetItem("12", "Κοινωνικές εισφορές", "REVENUE", 56_000_000L));
+        items.add(new BudgetItem("21", "Παροχές σε εργαζομένους", "EXPENDITURE", 14_849_625_000L));
+
+        Budget budget = new Budget(2024, items);
+
         double result = budgetService.calculateSurplusOrDeficit(budget);
-        assertEquals(7_000_000_000.0, result, 0.001, "Το πλεόνασμα πρέπει να είναι 7 δισεκατομμύρια");
+
+        assertEquals(41_803_375_000.0, result, 0.001, "Ο υπολογισμός πλεονάσματος δεν είναι σωστός");
     }
-    
 }
