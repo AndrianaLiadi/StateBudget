1e98de6808da5ed04c933ce325d789cf1f9d8329
Pantelis Galanos <t8240023@aueb.gr>
2026-01-03 16:51:20 +0200

    Test tis BudgetItemTest

--- src/test/java/model/BudgetItemTest.java
+++ src/test/java/model/BudgetItemTest.java
@@ -4,8 +4,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.util.List;
-
 class BudgetItemTest {       
 
     private BudgetItem parent;
